26119f720d0f8e57ee0ef2a5a22121097f0a7c4b
AlanCisneros1007 <47546322+AlanCisneros1007@users.noreply.github.com>
2020-11-08 19:28:59 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -9,13 +9,14 @@ using namespace std;
 int opcion;
 char narch[60];
 int main() {
-	int cv,opcion1, opcion2, opcion3, opcion4;
+	int cv,opcion1, opcion2, opcion3, opcion4,noUsu;
 	ofstream archivo;
-	char buscarPorProveedor[30] = "";
+	char buscarPorProveedor[30] = "", mensaje[40] = "ERROR: Introduce la opcion a realizar: ";
 	setlocale(LC_ALL, "spanish");
 
-	cout << "'\n------------------------------------------------------------\n";
-	cout << "PRIMERA PARTE DE PROYECTO FINAL C++\n";
+	cout << "----------------------------------------------------------\n";
+	cout << "\tPRIMERA PARTE DE PROYECTO FINAL C++\n";
+	cout << "----------------------------------------------------------\n";
 	cout << "\nMENU. ";
 
 	do {
@@ -23,18 +24,19 @@ int main() {
 		/*	cout << "\n\nOpcion: ";
 			cin >> opcion;
 			cin.ignore();*/
-		opcion = pedirOpcion();
+		strcpy_s(mensaje, "ERROR: Introduce la opcion a realizar: ");
+		opcion = pedirOpcion(1,6, mensaje);
 		switch (opcion) {
 		case 1:
-
 				cout << "Ingrese el nombre del archivo con los registros: ";
 				cin.getline(narch, 60, '\n');
 				Lectura(narch);
+			//	imprimirRegistros();
 				break;
 			
 		case 2:
 			cout << "BAJA DE REGISTROS" << "\n";
-			cout << "Ingrese la clave de la maquina que dara de baja: ";
+			cout << "Ingrese la clave de la maquina que dara de baja [xxxx]: ";
 			cin >> cv;
 			cin.ignore();
 			Baja(cv);
@@ -42,7 +44,7 @@ int main() {
 		case 3:
 			cout << "\nLocaliza Registro.\n";
 			cout << "1. Buscar por clave maquina.\n2. Buscar por Nombre-Proveedor.\n3. Salir.\n";	
-			opcion2 = pedirOpcion();
+			opcion2 = pedirOpcion(1,3,mensaje);
 			switch (opcion2) {
 			case 1:
 				cout << "Ingresa la clave de la maquina: ";
@@ -69,9 +71,9 @@ int main() {
 			break;
 		case 4:
 			cout << "\nREPORTES.\n";
-			cout << "1. Reporte ordenado por clave maquina.\n2. Reporte ordenado por Nombre-Proveedor.\n3. Salir.\nIngresa la opcion deseada: ";
+			cout << "1. Reporte ordenado por clave maquina.\n2. Reporte ordenado por Nombre-Proveedor.\n3. Salir.\n";
 
-			opcion3 = pedirOpcion();
+			opcion3 = pedirOpcion(1,3, mensaje);
 			switch (opcion3) {
 			case 1:
 				OrdenamientoClave();
@@ -92,19 +94,17 @@ int main() {
 
 			break;
 		case 5:
-			int noUsu;
+			
 			cout << "\nARCHIVOS.\n";
-			cout << "1. Crear archivo de entrada al sistema.\n2. Crear archivo con la informacion ordenada por nombre-proveedor.\n3. Crear archivo con la informacion ordenada por clave-proveedor.\nIngresa la opcion deseada: ";
+			cout << "1. Crear archivo de entrada al sistema.\n2. Crear archivo con la informacion ordenada por nombre-proveedor.\n3. Crear archivo con la informacion ordenada por clave-proveedor.\n";
 
-			opcion4 = pedirOpcion();
+			opcion4 = pedirOpcion(1,3,mensaje);
 			switch (opcion4) {
 			case 1:
+				//strcpy_s(mensaje, "ERROR: Introduce un número valido: ");
 				cout << "Bienvenido, va a registrar usuarios en un TXT para poder usarlos en el sistema.\n¿Cuantos usuarios quiere guardar?. ";
-				cin >> noUsu;
-				cin.ignore();
+				noUsu = pedirOpcion(1, 50, mensaje);
 				registrarMaquinas(noUsu);
-			
-				cout << "FALTA DE REALIZAR ESTA PARTE" << endl;
 				break;
 
 			case 2:
@@ -123,7 +123,7 @@ int main() {
 			break;
 		case 6:
 			cout << "\n\nNos vemos pronto." << endl;
-			imprimirRegistros();
+	
 			exit(0);
 			break;
 		default:
